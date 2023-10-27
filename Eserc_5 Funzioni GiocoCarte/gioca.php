f182ff4563a175c5736f23c790e6ec0fb71339ef
MarcuccioArme <121099587+MarcuccioArme@users.noreply.github.com>
2023-10-27 09:49:13 +0200

    Aggiunti file
    Aggiunti file del progetto

---
+++ Eserc_5 Funzioni GiocoCarte/gioca.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Un gioco con le "carte"</title>
+</head>
+<body>
+
+    <h1>Un gioco con le "carte"</h1>
+
+    <form action="<?php echo($_SERVER["PHP_SELF"]) ?>" method="POST">
+
+        
+
+    </form>
+    
+</body>
+</html>
+
+<?php
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        
+    }
+
+?>
