ff1b4f4963d160a130fe630ee59d04758d581b46
Marco Armenise <armenise.marco@panettipitagora.edu.it>
2023-10-28 17:36:37 +0200

    Aggiunti file
    Aggiunto terzo esercizio

--- Eserc_5 Funzioni PHP/a.php
+++ Eserc_5 Funzioni PHP/a.php
@@ -4,9 +4,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>La Sentinella</title>
 </head>
 <body>
-    
+
+    <h1>La Sentinella</h1>
+
+    <form action="<?php echo($_SERVER["PHP_SELF"]) ?>" method="POST">
+        <input type="submit" name="submit" value="Genera">
+    </form>
+
 </body>
 </html>
+
+<?php
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $sentinella = rand(1,100);
+        $contatore = 0;
+    
+        echo "<br>La sentinella è: $sentinella <br><br>";
+    
+        while ($contatore < 10) {
+    
+            $randNum = rand(1,100);
+            echo "Il numero casuale è: $randNum <br>";
+    
+            if ($randNum > $sentinella) {
+                $contatore++;
+            }
+        }
+    
+        echo "<br>Hai trovato 10 numeri maggiori della sentinella!";
+
+    }
+
+?>
